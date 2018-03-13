34afb1af781616919e10684a6ec5e2624a20f465
Jaime <jcdiaz356@hotmasil.com>
2018-03-12 23:19:50 -0500

    Initial commit

---
+++ app/src/main/java/com/dataservicios/ttauditalicorpvisibilidad/util/GlobalConstant.java
@@ -0,0 +1,17 @@
+package com.dataservicios.ttauditalicorpvisibilidad.util;
+/**
+ * Created by usuario on 11/11/2014.
+ */
+public final class GlobalConstant {
+
+    public static String dominio = "http://ttaudit.com";
+    public static final String URL_USER_IMAGES = dominio + "/media/users/";
+    public static String directory_images = "/Pictures/" ;
+    public static final String JPEG_FILE_PREFIX = "_ttaudit_visi_";
+    public static final String JPEG_FILE_SUFFIX = ".jpg";
+    public static final String ALBUN_NAME = "visi_Photo";
+    public static final String ALBUN_NAME_TEMP = "visi_Photo_Temp";
+    public static final String ALBUN_NAME_BACKUP = "visi_Photo_Backup";
+
+}
+
